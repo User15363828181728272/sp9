7ca42f42d863855138ff80f0915bd430a0c35d2b
User15363828181728272 <defanaldrian1329@gmail.com>
2026-03-23 14:02:02 +0700

    upload: api/config.js

---
+++ api/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  ptero: {
+    domain: 'https://defandryan.shanydev.web.id',
+    plta:   'ptla_4CarLMQG8jYgONUOlDMntT0GcQTjnpJ7Ay1uAn2n8CU',
+    pltc:   'ptlc_rzH9eSfc0yripONRykpXbjz4q3djmOW3wkMIOwj259m',
+    server: '8c118cdb',
+    loc:    '1',
+    eggs:   '15',
+    path:   '/home/container/data',
+  },
+  jwt: {
+    secret: 'scure-pgsk-lixhwywvdjj622-622026',
+  },
+  admin: {
+    username: 'admin',
+    password: 'admin123',
+  },
+};
